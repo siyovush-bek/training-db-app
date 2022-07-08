00f22582996a0ea79d183a09183646485f500519
Siyovushbek <siyovusht@gmail.com>
2022-07-08 23:07:09 +0300

    ExerciseRecordsActivity and ExercisesActivity created and wired with buttons

---
+++ app/src/main/java/uz/siyovushbek/mytrainingdb/ExerciseRecordsActivity.java
@@ -0,0 +1,14 @@
+package uz.siyovushbek.mytrainingdb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExerciseRecordsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_records);
+    }
+}
---
+++ app/src/main/java/uz/siyovushbek/mytrainingdb/ExercisesActivity.java
@@ -0,0 +1,14 @@
+package uz.siyovushbek.mytrainingdb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExercisesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercises);
+    }
+}
